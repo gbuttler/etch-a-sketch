0781eea7689d9419aa439a1221df0c9e525cc33b
Grace <141918570+gbuttler@users.noreply.github.com>
2023-10-25 17:31:23 +0100

    made a 16x16 grid with responsive even squares

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-var div = document.createElement('div', 'square')
-// div.id = 'square'
+// var div = document.createElement('div')
+// div.setAttribute("class", "square");
     
-document.getElementById('container').appendChild(div);
+// document.getElementById('container').appendChild(div);
 
-console.log(div) 
+
+// console.log(div) 
+
+
+// var test = document.createElement('h3')
+// test.textContent = ('hello world');
+// document.getElementsByClassName('square')[0].appendChild(test)
+
+function createDivs() {
+    for (let i = 0; i < 256; i++) {
+        var div = document.createElement('div')
+        div.setAttribute("class", "square");
+    
+        document.getElementById('container').appendChild(div);
+        console.log(div) 
+    }
+}
+
+console.log(createDivs());
