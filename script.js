1e43d7bab44b3a01937b1ccb4c0999a91ceb25bb
Grace <141918570+gbuttler@users.noreply.github.com>
2023-10-26 10:23:12 +0100

    get the squares to change colour as I hover over them

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-// var div = document.createElement('div')
-// div.setAttribute("class", "square");
-    
-// document.getElementById('container').appendChild(div);
-
-
-// console.log(div) 
-
-
-// var test = document.createElement('h3')
-// test.textContent = ('hello world');
-// document.getElementsByClassName('square')[0].appendChild(test)
-
 function createDivs() {
     for (let i = 0; i < 256; i++) {
         var div = document.createElement('div')
         div.setAttribute("class", "square");
-    
+        div.addEventListener("mouseover", function() {
+            return (this.style.backgroundColor = 'red');
+        });
         document.getElementById('container').appendChild(div);
         console.log(div) 
     }
 }
 
-console.log(createDivs());
+// console.log(createDivs());
+
+createDivs()
