405f235d04ab37261c8a5ab5fccbcbf3522324af
Grace <141918570+gbuttler@users.noreply.github.com>
2023-10-26 12:43:17 +0100

    make the grid dynamic

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-function createDivs() {
-    for (let i = 0; i < 256; i++) {
-        var div = document.createElement('div')
-        div.setAttribute("class", "square");
-        div.addEventListener("mouseover", function() {
-            return (this.style.backgroundColor = 'red');
-        });
-        document.getElementById('container').appendChild(div);
-        console.log(div) 
-    }
-}
 
-// console.log(createDivs());
+    function createDivs() {
+        for (let i = 0; i < 256; i++) {
+            var div = document.createElement('div')
+            div.setAttribute("class", "square");
+            div.addEventListener("mouseover", function() {
+                return (this.style.backgroundColor = 'red');
+            });
+            document.getElementById('container').appendChild(div);
+            // console.log(div) 
+        }
+    }
+
+const grid = document.getElementById('container');
+
+grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
+
+
 
 createDivs()
+
+
+
+function gridSizeButtonClicked() {
+    let userNumber = prompt("Please enter a number between 1 and 100:");
+    console.log(userNumber);
+    const grid = document.getElementById('container');
+        while (grid.hasChildNodes()) {
+            grid.removeChild(grid.firstChild);
+        }
+    function createDivs() {
+        for (let i = 0; i < (userNumber*userNumber); i++) {
+            var div = document.createElement('div')
+            div.setAttribute("class", "square");
+            div.addEventListener("mouseover", function() {
+                return (this.style.backgroundColor = 'red');
+            });
+            document.getElementById('container').appendChild(div);
+            console.log(div) 
+        }
+        const grid = document.getElementById('container');
+
+        grid.style.gridTemplateColumns = `repeat( ${userNumber}, 1fr)`;
+
+    }
+    createDivs()
+}
+
+
